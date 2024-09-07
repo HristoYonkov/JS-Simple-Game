c197048e8b88ebabbfd0532a7eb5c5c29bcf8599
Hristo Yonkov <hristopturs@gmail.com>
2024-09-07 12:15:04 +0300

    fix box of the canvas

--- Retro-Tanks/index.js
+++ Retro-Tanks/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1000;
+    canvas.width = 750;
     canvas.height = 500;
 
     class InputHandler {
