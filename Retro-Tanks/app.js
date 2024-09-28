c8842c555e923210f3da35063473cbf22ca18d23
Hristo Yonkov <hristopturs@gmail.com>
2024-09-28 17:59:36 +0300

    create socket.io setup and connect it within the client

--- Retro-Tanks/app.js
+++ Retro-Tanks/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 const port = 3000;
 
 app.use(express.static('client'));
@@ -8,6 +14,10 @@ app.get('/', (req, res) => {
     res.sendFile(_dirname + '/index.html');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+})
+
+server.listen(port, () => {
     console.log(`Game is listening on port ${port}..`);
 })
